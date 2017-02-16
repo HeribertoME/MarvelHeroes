dc0669576b09b6217ae00e82924fe96789d8fa76
Heriberto Martinez Elizarraraz <hmelizarraraz@hotmail.com>
2017-02-15 19:41:53 -0600

    Se implementa retrofit

--- app/src/main/java/com/hmelizarraraz/marvelheroes/MainActivity.java
+++ app/src/main/java/com/hmelizarraraz/marvelheroes/MainActivity.java
@@ -2,6 +2,18 @@ package com.hmelizarraraz.marvelheroes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.hmelizarraraz.marvelheroes.API.MarvelService;
+import com.hmelizarraraz.marvelheroes.Models.Basic;
+import com.hmelizarraraz.marvelheroes.Models.Data;
+import com.hmelizarraraz.marvelheroes.Models.SuperHero;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Call<Basic<Data<ArrayList<SuperHero>>>> superHeroesCall = MarvelService.getMarvelAPI().getHeroes(354);
+
+        superHeroesCall.enqueue(new Callback<Basic<Data<ArrayList<SuperHero>>>>() {
+            @Override
+            public void onResponse(Call<Basic<Data<ArrayList<SuperHero>>>> call, Response<Basic<Data<ArrayList<SuperHero>>>> response) {
+
+                Toast.makeText(MainActivity.this, "Heroe: " + response.body().getData().getResults().get(2).getName(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<Basic<Data<ArrayList<SuperHero>>>> call, Throwable t) {
+
+                Toast.makeText(MainActivity.this, "Error en la llamada", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
